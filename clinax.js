8eaae5b6faaba823873960dfc2f9fc43c938ce14
Dannydramo <alumandudaniel217@gmail.com>
2022-09-30 18:43:14 -0700

    first commit

---
+++ clinax.js
@@ -0,0 +1,6 @@
+const deleteBtn = document.querySelector('.delete');
+const removeItem = document.querySelector('.container7');
+
+deleteBtn.addEventListener('click', () =>{
+    removeItem.style.display = 'none';
+})
